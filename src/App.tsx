25d4c28c59bcb2ac5a962cf3596bc8829e6e501b
ArthurGoupil <arthurgoupil@gmail.com>
2020-09-29 17:33:57 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './styles/reset.css';
+import './styles/index.css';
+
+import styled from 'styled-components';
+
+const App = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Root>
+      <Header className='d-flex justify-center align-center'>
+        <Logo src='/glaglacar-logo.png' alt='Logo GlaGlaCar' />
+      </Header>
+    </Root>
   );
-}
+};
 
 export default App;
+
+const Root = styled.div`
+  background-image: url('/glaglacar-background.svg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Header = styled.header.attrs((props: { className: string }) => ({
+  className: props.className,
+}))`
+  height: 80px;
+  background-color: white;
+  box-shadow: 0 10px 15px rgba(0, 0, 0, 0.1);
+`;
+
+const Logo = styled.img.attrs((props: { src: string; alt: string }) => ({
+  src: props.src,
+  alt: props.alt,
+}))`
+  height: 50px;
+`;
